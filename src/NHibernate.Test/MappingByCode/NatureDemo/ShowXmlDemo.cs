84e7082a3e2c44a4d6ae36cc4adaa639c4e43e33
Roman Artiukhin <bahusdrive@gmail.com>
2017-12-26 22:08:25 +0530

    temp

--- src/NHibernate.Test/MappingByCode/NatureDemo/ShowXmlDemo.cs
+++ src/NHibernate.Test/MappingByCode/NatureDemo/ShowXmlDemo.cs
@@ -10,10 +10,39 @@ namespace NHibernate.Test.MappingByCode.NatureDemo
 	[TestFixture]
 	public class ShowXmlDemo
 	{
+		private static long _allocated;
+
 		[Test, Explicit]
 		public void MappingAndShowXmlInConsole()
 		{
 			Console.Write(GetMapping().AsString());
+		}
+
+		[Test, Explicit]
+		public void TestShowInConsole()
+		{
+			_allocated = ShowMemoryTaken("initial :");
+			
+			var cfg = TestConfigurationHelper.GetDefaultConfiguration();
+			cfg.AddMapping(GetMapping());
+			foreach (var i in Enumerable.Range(0, 1000))
+			{
+				using (var factory = cfg.BuildSessionFactory())
+				{
+					ShowMemoryTaken($"[{i}] Before disposing: ");
+				}
+				GC.Collect();
+
+				ShowMemoryTaken($"[{i}] After disposing: ");
+			}
+			
+		}
+
+		private static long ShowMemoryTaken(string comment)
+		{
+			var totalMemory = GC.GetTotalMemory(true);
+			Console.Error.WriteLine(comment + ((totalMemory - _allocated) / 1024.0).ToString("##.###") + "Kb");
+			return totalMemory;
 		}
 
 		public static HbmMapping GetMapping()
@@ -29,7 +58,7 @@ namespace NHibernate.Test.MappingByCode.NatureDemo
 				comp.ManyToOne(address => address.StateProvince);
 			});
 
-			mapper.Class<Animal>(rc => 
+			mapper.Class<Animal>(rc =>
 			{
 				rc.Id(x => x.Id, map => map.Generator(Generators.Native));
 
@@ -37,82 +66,82 @@ namespace NHibernate.Test.MappingByCode.NatureDemo
 				rc.Property(animal => animal.BodyWeight);
 				rc.ManyToOne(animal => animal.Mother);
 				rc.ManyToOne(animal => animal.Father);
-				rc.ManyToOne(animal => animal.Zoo);
+				//rc.ManyToOne(animal => animal.Zoo);
 				rc.Property(animal => animal.SerialNumber);
 				rc.Set(animal => animal.Offspring, cm => cm.OrderBy(an => an.Father), rel => rel.OneToMany());
 			});
 
-			mapper.JoinedSubclass<Reptile>(jsc => { jsc.Property(reptile => reptile.BodyTemperature); });
+			//mapper.JoinedSubclass<Reptile>(jsc => { jsc.Property(reptile => reptile.BodyTemperature); });
 
-			mapper.JoinedSubclass<Lizard>(jsc => { });
+			//mapper.JoinedSubclass<Lizard>(jsc => { });
 
-			mapper.JoinedSubclass<Mammal>(jsc =>
-			{
-				jsc.Property(mammal => mammal.Pregnant);
-				jsc.Property(mammal => mammal.Birthdate);
-			});
+			//mapper.JoinedSubclass<Mammal>(jsc =>
+			//{
+			//	jsc.Property(mammal => mammal.Pregnant);
+			//	jsc.Property(mammal => mammal.Birthdate);
+			//});
 
-			mapper.JoinedSubclass<DomesticAnimal>(jsc =>
-			                                      {
-			                                      	jsc.ManyToOne(domesticAnimal => domesticAnimal.Owner);
-			                                      });
+			//mapper.JoinedSubclass<DomesticAnimal>(jsc =>
+			//                                      {
+			//                                      	jsc.ManyToOne(domesticAnimal => domesticAnimal.Owner);
+			//                                      });
 
-			mapper.JoinedSubclass<Cat>(jsc => { });
+			//mapper.JoinedSubclass<Cat>(jsc => { });
 
-			mapper.JoinedSubclass<Dog>(jsc => { });
+			//mapper.JoinedSubclass<Dog>(jsc => { });
 
-			mapper.JoinedSubclass<Human>(jsc =>
-			{
-				jsc.Component(human => human.Name, comp =>
-				{
-					comp.Property(name => name.First);
-					comp.Property(name => name.Initial);
-					comp.Property(name => name.Last);
-				});
-				jsc.Property(human => human.NickName);
-				jsc.Property(human => human.Height);
-				jsc.Property(human => human.IntValue);
-				jsc.Property(human => human.FloatValue);
-				jsc.Property(human => human.BigDecimalValue);
-				jsc.Property(human => human.BigIntegerValue);
-				jsc.Bag(human => human.Friends, cm => { }, rel => rel.ManyToMany());
-				jsc.Map(human => human.Family, cm => { }, rel => rel.ManyToMany());
-				jsc.Bag(human => human.Pets, cm => { cm.Inverse(true); }, rel => rel.OneToMany());
-				jsc.Set(human => human.NickNames, cm =>
-				{
-					cm.Lazy(CollectionLazy.NoLazy);
-					cm.Sort();
-				}, cer => { });
-				jsc.Map(human => human.Addresses, cm => { }, rel => rel.Component(comp => { }));
-			});
+			//mapper.JoinedSubclass<Human>(jsc =>
+			//{
+			//	jsc.Component(human => human.Name, comp =>
+			//	{
+			//		comp.Property(name => name.First);
+			//		comp.Property(name => name.Initial);
+			//		comp.Property(name => name.Last);
+			//	});
+			//	jsc.Property(human => human.NickName);
+			//	jsc.Property(human => human.Height);
+			//	jsc.Property(human => human.IntValue);
+			//	jsc.Property(human => human.FloatValue);
+			//	jsc.Property(human => human.BigDecimalValue);
+			//	jsc.Property(human => human.BigIntegerValue);
+			//	jsc.Bag(human => human.Friends, cm => { }, rel => rel.ManyToMany());
+			//	jsc.Map(human => human.Family, cm => { }, rel => rel.ManyToMany());
+			//	jsc.Bag(human => human.Pets, cm => { cm.Inverse(true); }, rel => rel.OneToMany());
+			//	jsc.Set(human => human.NickNames, cm =>
+			//	{
+			//		cm.Lazy(CollectionLazy.NoLazy);
+			//		cm.Sort();
+			//	}, cer => { });
+			//	jsc.Map(human => human.Addresses, cm => { }, rel => rel.Component(comp => { }));
+			//});
 
-			mapper.Class<User>(rc =>
-			{
-				rc.Id(u => u.Id, im => im.Generator(Generators.Foreign<User>(u => u.Human)));
+			//mapper.Class<User>(rc =>
+			//{
+			//	rc.Id(u => u.Id, im => im.Generator(Generators.Foreign<User>(u => u.Human)));
 
-				rc.Property(user => user.UserName);
-				rc.OneToOne(user => user.Human, rm => rm.Constrained(true));
-				rc.List(user => user.Permissions, cm => { }, cer => { });
-			});
+			//	rc.Property(user => user.UserName);
+			//	rc.OneToOne(user => user.Human, rm => rm.Constrained(true));
+			//	rc.List(user => user.Permissions, cm => { }, cer => { });
+			//});
 
-			mapper.Class<Zoo>(rc =>
-			{
-				rc.Id(x => x.Id, map => map.Generator(Generators.Native));
-				rc.Property(zoo => zoo.Name);
-				rc.Property(zoo => zoo.Classification);
-				rc.Map(zoo => zoo.Mammals, cm => { }, rel => rel.OneToMany());
-				rc.Map(zoo => zoo.Animals, cm => { cm.Inverse(true); }, rel => rel.OneToMany());
-				rc.Component(zoo => zoo.Address, comp => { });
-			});
+			//mapper.Class<Zoo>(rc =>
+			//{
+			//	rc.Id(x => x.Id, map => map.Generator(Generators.Native));
+			//	rc.Property(zoo => zoo.Name);
+			//	rc.Property(zoo => zoo.Classification);
+			//	rc.Map(zoo => zoo.Mammals, cm => { }, rel => rel.OneToMany());
+			//	rc.Map(zoo => zoo.Animals, cm => { cm.Inverse(true); }, rel => rel.OneToMany());
+			//	rc.Component(zoo => zoo.Address, comp => { });
+			//});
 
-			mapper.Subclass<PettingZoo>(sc => { });
+			//mapper.Subclass<PettingZoo>(sc => { });
 
-			mapper.Class<StateProvince>(rc =>
-			{
-				rc.Id(x => x.Id, map => map.Generator(Generators.Native));
-				rc.Property(sp => sp.Name);
-				rc.Property(sp => sp.IsoCode);
-			});
+			//mapper.Class<StateProvince>(rc =>
+			//{
+			//	rc.Id(x => x.Id, map => map.Generator(Generators.Native));
+			//	rc.Property(sp => sp.Name);
+			//	rc.Property(sp => sp.IsoCode);
+			//});
 			return mapper.CompileMappingFor(typeof (Animal).Assembly.GetTypes().Where(t => t.Namespace == typeof (Animal).Namespace));
 		}
 	}
