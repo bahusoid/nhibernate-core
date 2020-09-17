ceda0138ae0425ff105c90ea22808af393f6c0f6
Roman Artiukhin <bahusdrive@gmail.com>
2020-09-17 10:10:11 +0300

    Test case

---
+++ src/NHibernate.Test/NHSpecificTest/NH3242/DomainClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NHibernate.Test.NHSpecificTest.NH3242
+{
+	public class Booking
+	{
+		public virtual int BookingID { get; set; }
+		public virtual BookingTypes Type { get; set; }
+		public virtual DateTime DateBooked { get; set; }
+	}
+
+	public enum BookingTypes
+	{
+		User = 1,
+		Guest = 2
+	}
+
+	public class BookingGuest : Booking
+	{
+		public virtual string FullName { get; set; }
+	}
+
+	public class BookingUser : Booking
+	{
+		public virtual User User { get; set; }
+	}
+
+	public class User
+	{
+		public virtual int UserID { get; set; }
+		public virtual string FullName { get; set; }
+		public virtual IList<BookingUser> Bookings { get; set; } = new List<BookingUser>();
+	}
+}
---
+++ src/NHibernate.Test/NHSpecificTest/NH3242/SampleTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NHibernate.Dialect;
+using NHibernate.Linq;
+using NUnit.Framework;
+
+namespace NHibernate.Test.NHSpecificTest.NH3242
+{
+	[TestFixture]
+	public class SampleTest : BugTestCase
+	{
+		protected override void OnSetUp()
+		{
+			base.OnSetUp();
+			using (ISession session = this.OpenSession())
+			{
+				var user = new User() {FullName = "Bob Smith"};
+				session.Save(user);
+
+				var booking = new BookingUser() {Type = BookingTypes.User, DateBooked = DateTime.UtcNow.AddDays(-1), User = user};
+				session.Save(booking);
+
+				session.Flush();
+			}
+		}
+
+		protected override void OnTearDown()
+		{
+			base.OnTearDown();
+			using (ISession session = this.OpenSession())
+			{
+				string hql = "from System.Object";
+				session.Delete(hql);
+				session.Flush();
+			}
+		}
+
+		protected override bool AppliesTo(NHibernate.Dialect.Dialect dialect)
+		{
+			return dialect as MsSql2005Dialect != null;
+		}
+
+		[Test]
+		public void JoinOnBasePropertyShouldWork()
+		{
+			using (new SqlLogSpy())
+			using (ISession session = this.OpenSession())
+			{
+				var users = session.Query<User>().Where(u => u.Bookings.Count(b => b.DateBooked <= DateTime.UtcNow) == 1).ToList();
+
+				Assert.AreEqual(1, users.Count);
+			}
+		}
+	}
+}
