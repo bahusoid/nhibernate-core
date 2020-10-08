009416e82b70a49c882f47257fe6b0d1e271c085
Roman Artiukhin <bahusdrive@gmail.com>
2020-10-08 21:56:14 +0300

    Test case

---
+++ src/NHibernate.Test/NHSpecificTest/GH2580/Entity.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections.Generic;
+using NHibernate.Mapping.ByCode;
+using NHibernate.Mapping.ByCode.Conformist;
+
+namespace NHibernate.Test.NHSpecificTest.GH2580
+{
+	using Cascade = NHibernate.Mapping.ByCode.Cascade;
+
+	public class FamilyEntity
+	{
+		public virtual long Id { get; set; }
+		public virtual ICollection<PersonEntity> Persons { get; set; }
+	}
+
+	public class FamilyMap : ClassMapping<FamilyEntity>
+	{
+		public FamilyMap()
+		{
+			Table("Families");
+
+			Id(
+				p => p.Id,
+				id =>
+				{
+					id.Column("Family_Id");
+					id.Generator(Generators.Increment);
+				});
+
+			Bag(
+				p => p.Persons,
+				bag =>
+				{
+					bag.Inverse(true);
+					bag.Cascade(Cascade.All.Include(Cascade.DeleteOrphans));
+					bag.Lazy(CollectionLazy.NoLazy);
+					bag.Key(k => k.Column("Family_Id"));
+				},
+				a => a.OneToMany());
+		}
+	}
+
+	public class PersonEntity
+	{
+		public virtual long Id { get; set; }
+
+		private FamilyEntity _family;
+
+		public virtual FamilyEntity Family
+		{
+			get { return _family; }
+			set
+			{
+				_family?.Persons.Remove(this);
+				_family = value;
+				_family?.Persons.Add(this);
+			}
+		}
+	}
+
+	public class PersonMap : ClassMapping<PersonEntity>
+	{
+		public PersonMap()
+		{
+			Table("Persons");
+
+			Id(
+				p => p.Id,
+				id =>
+				{
+					id.Column("Person_Id");
+					id.Generator(Generators.Increment);
+				});
+
+			ManyToOne(
+				p => p.Family,
+				r =>
+				{
+					r.Lazy(LazyRelation.NoLazy);
+					r.Access(Accessor.Field);
+					r.Column("Family_Id");
+					r.NotNullable(true);
+				});
+		}
+	}
+
+	public class AdultEntity : PersonEntity
+	{
+	}
+
+	public class AdultMap : JoinedSubclassMapping<AdultEntity>
+	{
+		public AdultMap()
+		{
+			Table("Adults");
+			Key(prop => prop.Column("Person_Id"));
+		}
+	}
+
+	public class ChildEntity : PersonEntity
+	{
+	}
+
+	public class ChildMap : JoinedSubclassMapping<ChildEntity>
+	{
+		public ChildMap()
+		{
+			Table("Children");
+			Key(prop => prop.Column("Person_Id"));
+		}
+	}
+}
---
+++ src/NHibernate.Test/NHSpecificTest/GH2580/FixtureByCode.cs
@@ -0,0 +1,58 @@
+﻿using System.Linq;
+using NHibernate.Cfg.MappingSchema;
+using NHibernate.Mapping.ByCode;
+using NUnit.Framework;
+
+namespace NHibernate.Test.NHSpecificTest.GH2580
+{
+	[TestFixture]
+	public class JoinOnSubclassIssue : TestCaseMappingByCode
+	{
+		protected override HbmMapping GetMappings()
+		{
+			var mapper = new ModelMapper();
+			mapper.AddMapping<FamilyMap>();
+			mapper.AddMapping<PersonMap>();
+			mapper.AddMapping<AdultMap>();
+			mapper.AddMapping<ChildMap>();
+
+			return mapper.CompileMappingForAllExplicitlyAddedEntities();
+		}
+
+		protected override void OnSetUp()
+		{
+			using (var session = OpenSession())
+			using (var transaction = session.BeginTransaction())
+			{
+				transaction.Commit();
+			}
+		}
+
+		protected override void OnTearDown()
+		{
+			using (var session = OpenSession())
+			using (var transaction = session.BeginTransaction())
+			{
+				session.CreateQuery("delete from System.Object").ExecuteUpdate();
+
+				transaction.Commit();
+			}
+		}
+
+		[Test]
+		public void JoinOnSubclassWithBaseTableReferenceInOnClause()
+		{
+			using(new SqlLogSpy())
+			using (var session = OpenSession())
+			{
+				var ids = (from adult in session.Query<AdultEntity>()
+							join child in session.Query<ChildEntity>()
+								on adult.Family.Id equals child.Family.Id
+							where adult.Id == 1
+							select child.Id)
+						.Distinct()
+						.ToList();
+			}
+		}
+	}
+}
