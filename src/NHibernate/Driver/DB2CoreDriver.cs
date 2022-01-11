17984137f079caf557dfe203c1ef5790e8b4ff91
Roman Artiukhin <bahusdrive@gmail.com>
2022-01-11 12:13:08 +0200

    Support NET5.IBM.Data.DB2 lib

--- src/NHibernate/Driver/DB2CoreDriver.cs
+++ src/NHibernate/Driver/DB2CoreDriver.cs
@@ -1,17 +1,34 @@
+using System;
 using System.Data.Common;
+using System.Reflection;
 using NHibernate.SqlTypes;
 
 namespace NHibernate.Driver
 {
 	/// <summary>
-	/// A NHibernate Driver for using the IBM.Data.DB2.Core DataProvider.
+	/// A NHibernate Driver for using the IBM.Data.DB2.Core or NET5.IBM.Data.DB2 DataProvider.
 	/// </summary>
 	public class DB2CoreDriver : DB2DriverBase
-	{		
-		public DB2CoreDriver() : base("IBM.Data.DB2.Core")
+	{
+		private static readonly string _assemblyName = "IBM.Data.DB2.Core";
+
+		static DB2CoreDriver()
+		{
+			try
+			{
+				var net5AssemblyName = "IBM.Data.Db2";
+				Assembly.Load(net5AssemblyName);
+				_assemblyName = net5AssemblyName;
+			}
+			catch
+			{
+			}
+		}
+
+		public DB2CoreDriver() : base(_assemblyName)
 		{
 		}
-		
+
 		public override bool UseNamedPrefixInSql => true;
 
 		public override bool UseNamedPrefixInParameter => true;
