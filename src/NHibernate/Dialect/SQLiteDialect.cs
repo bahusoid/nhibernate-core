735967b3554b97f28b4eb91a2e96c48de9e43970
Roman Artiukhin <bahusdrive@gmail.com>
2023-03-14 16:12:45 +0200

    Remove transparentcast ovveride

--- src/NHibernate/Dialect/SQLiteDialect.cs
+++ src/NHibernate/Dialect/SQLiteDialect.cs
@@ -126,9 +126,6 @@ namespace NHibernate.Dialect
 			// http://sqlite.1065341.n5.nabble.com/XOR-operator-td98004.html
 			RegisterFunction("bxor", new SQLFunctionTemplate(null, "((?1 | ?2) - (?1 & ?2))"));
 
-			// NH-3787: SQLite requires the cast in SQL too for not defaulting to string.
-			RegisterFunction("transparentcast", new CastFunction());
-
 			if (_binaryGuid)
 				RegisterFunction("strguid", new SQLFunctionTemplate(NHibernateUtil.String, "substr(hex(?1), 7, 2) || substr(hex(?1), 5, 2) || substr(hex(?1), 3, 2) || substr(hex(?1), 1, 2) || '-' || substr(hex(?1), 11, 2) || substr(hex(?1), 9, 2) || '-' || substr(hex(?1), 15, 2) || substr(hex(?1), 13, 2) || '-' || substr(hex(?1), 17, 4) || '-' || substr(hex(?1), 21) "));
 			else
